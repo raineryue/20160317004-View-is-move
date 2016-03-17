3c39f069042ca18cb1e80c6bb743e9564d4e1297
raineryue <it_jinlei.yue@163.com>
2016-03-17 17:29:00 +0800

    Initial Commit

---
+++ 20160317004-View is move/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160317004-View is move
+//
+//  Created by Rainer on 16/3/17.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 20160317004-View is moveTests/_0160317004_View_is_moveTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160317004_View_is_moveTests.m
+//  20160317004-View is moveTests
+//
+//  Created by Rainer on 16/3/17.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160317004_View_is_moveTests : XCTestCase
+
+@end
+
+@implementation _0160317004_View_is_moveTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
